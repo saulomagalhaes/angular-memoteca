39c509d09a106159effc8ab55a207f55dd1ee126
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-12-26 15:00:50 -0300

    feat(editar-pensamento): :sparkles: cria componente editar pensamento

---
+++ src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+
+@Component({
+  selector: 'app-editar-pensamento',
+  templateUrl: './editar-pensamento.component.html',
+  styleUrls: ['./editar-pensamento.component.css']
+})
+export class EditarPensamentoComponent {
+  pensamento: Pensamento = {
+    id: 0,
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route : ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    this.service.buscarPorId(Number(id)).subscribe((pensamento) => {
+      this.pensamento = pensamento;
+    });
+  }
+
+  editarPensamento() {
+    if (this.pensamento.id) {
+      this.service.editar(this.pensamento).subscribe(() => {
+        this.router.navigate(['/listarPensamento']);
+      });
+    }
+  }
+
+  cancelar() {
+    this.router.navigate(['/listarPensamento']);
+  }
+}
